0df268b8152f543f35d23318f037575fc21ddfeb
muri <106119896+mur1@users.noreply.github.com>
2023-09-10 17:21:55 -0300

    Create emoji.lua

---
+++ emoji.lua
@@ -0,0 +1,20 @@
+local cool = {
+    3142174173,
+    305672295,
+    1
+}
+
+local url = game:HttpGet("https://raw.githubusercontent.com/mur1/paysandu/main/whitelist.lua")
+local success, err = pcall(function()
+    for i,v in pairs(game.Players:GetChildren()) do
+        if v.Character then
+            if table.find(cool,v.UserId) then
+                v.Character.Humanoid.DisplayName = "[👑]"..v.DisplayName
+            elseif string.find(url,v.UserId) then
+                v.Character.Humanoid.DisplayName = "[⭐]"..v.DisplayName
+            elseif not v.Character.Head:FindFirstChild("OriginalSize") then
+                v.Character.Humanoid.DisplayName = "[🔒]"..v.DisplayName
+            end
+        end
+    end
+end)
