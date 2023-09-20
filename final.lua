a9dc5a832e88d152c3ca3f70179dfd15660df63e
muri <106119896+mur1@users.noreply.github.com>
2023-09-19 21:06:09 -0300

    Add files via upload

---
+++ final.lua
@@ -0,0 +1,15 @@
+-- // Final.Lua // --
+-- > Status: Working W
+-- // NeverBlade.Lua // --
+
+script.config = {
+   ajuste = 3.7,
+   range_do_hit = 0.5,
+
+   mode = 'Always',
+   deflect_type = 'Remote',
+   notifications = false,
+   keybind = Enum.KeyCode.E
+}
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/mur1/paysandu/main/obf.lua')()
